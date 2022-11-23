e41a8d6671e7f2fe3931982b6d6ed2871d14d604
Fonya Brandone <aminbrandone10@gmail.com>
2022-11-23 22:44:56 +0100

    Math Solver tool implemented
    Implemented the WikkiLearn math solver tool for solving basic Math equations and others

---
+++ math-solver/script/menu-nav.js
@@ -0,0 +1,9 @@
+//Open menu script:
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100% ";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0% ";
+}
