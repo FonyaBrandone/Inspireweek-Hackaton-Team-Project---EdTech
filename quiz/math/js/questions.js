5dc06239182e12a7c5eee647432aadf9b5a7f043
Jenna16 <118623943+Jenna16@users.noreply.github.com>
2022-11-23 17:13:02 +0100

    Add files via upload

---
+++ quiz/math/js/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+        numb: 1,
+        question: "Solve 300 - 150 x 2",
+        answer: "0",
+        options: [
+            "300",
+            "0",
+            "50",
+            "150"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What's the missing number in the sequence: 3, 6, 18, 72, _",
+        answer: "360",
+        options: [
+            "216",
+            "144",
+            "360",
+            "288"
+        ]
+    },
+    {
+        numb: 3,
+        question: "How many prime numbers are there from 1 to 30",
+        answer: "10",
+        options: [
+            "7",
+            "10",
+            "11",
+            "13"
+        ]
+    },
+    {
+        numb: 4,
+        question: "How many seconds are there in a week",
+        answer: "604,800",
+        options: [
+            "604,800",
+            "49,000",
+            "144,000",
+            "760,000"
+        ]
+    },
+    {
+        numb: 5,
+        question: "How many months are there in 120days, on average ?",
+        answer: "4months",
+        options: [
+            "5months",
+            "4months",
+            "3months",
+            "6months"
+        ]
+    },
+
+    {
+        numb: 6,
+        question: "Giving 9^(X) =  81. Find the value of X ? ",
+        answer: "2",
+        options: [
+            "3",
+            "2",
+            "4",
+            "9"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Evaluate 3(Y + 2) = 11, find the value of Y ?",
+        answer: "3",
+        options: [
+            "2",
+            "6",
+            "4",
+            "3"
+        ]
+    },
+    {
+        numb: 8,
+        question: "A right angle triangle has angles 32 and 128, what is the missing angle ?",
+        answer: "20",
+        options: [
+            "200",
+            "90",
+            "20",
+            "270"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What is the range of an Obtuse angle ?",
+        answer: "Between 90 and 180",
+        options: [
+            "Between 0 and 180",
+            "Between 90 and 180",
+            "Between 90 and 360",
+            "Between 0 and 90"
+        ]
+    },
+    {
+        numb: 10,
+        question: "The peimeter round a circle ?",
+        answer: "Circumference",
+        options: [
+            "Circumference",
+            "Radius",
+            "Volume",
+            "Diameter"
+        ]
+    },
+];
---
+++ quiz/physics/js/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+        numb: 1,
+        question: "Who came up with the theory of relativity ?",
+        answer: "Albert Einstein",
+        options: [
+            "Isaac Newton",
+            "Galileo Galilee",
+            "Nikola Tesla",
+            "Albert Einstein"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What are vector quantities ?",
+        answer: "Quantities having both magnitude and directions",
+        options: [
+            "Quantities having both magnitude and directions",
+            "These are quantities with no directions",
+            "Quantities having magnitude but no directions",
+            "None of the above"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the SI Unit of Energy ?",
+        answer: "Joule",
+        options: [
+            "Newton per metre",
+            "Pascal",
+            "Joule",
+            "Newton"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which quantity below is a vector quantity ?",
+        answer: "Velocity",
+        options: [
+            "Volume",
+            "Velocity",
+            "Mass",
+            "Speed"
+        ]
+    },
+    {
+        numb: 5,
+        question: "It is the reluctancy of a body to change it's state of motion.",
+        answer: "Inertia",
+        options: [
+            "Pressure",
+            "Inertia",
+            "Calibration",
+            "Momentum"
+        ]
+    },
+
+    {
+        numb: 6,
+        question: "The SI Unit of distance ?",
+        answer: "m",
+        options: [
+            "Km",
+            "m",
+            "Km",
+            "Mile"
+        ]
+    },
+    {
+        numb: 7,
+        question: "A body will only move when ?",
+        answer: "Force of friction < Applied force",
+        options: [
+            "Force of friction < Applied force",
+            "Force of friction = Applied force",
+            "Force of friction > Applied force",
+            "All of the above"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Fission means to ",
+        answer: "Break apart",
+        options: [
+            "Put together",
+            "Generate",
+            "Break apart",
+            "Solidify"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Which of the following satements about matter is correct ?",
+        answer: "Molecules of gases move freely than molecules of liquids and solids",
+        options: [
+            "Molecules of liquids move freely than molecules of solids and gases",
+            "Molecules of gases move freely than molecules of liquids and solids",
+            "Matter is weightless in time and in space",
+            "Molecules of solids move freely than molecules of liquids and gases"
+        ]
+    },
+    {
+        numb: 10,
+        question: "What is the level of current flowing through a lamp of 6 volts, with resistance of 3.5 ohms ?",
+        answer: "1.7A",
+        options: [
+            "0.6A",
+            "1.8A",
+            "21A",
+            "1.7A"
+        ]
+    },
+];
